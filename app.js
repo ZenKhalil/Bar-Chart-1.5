cebbf2f8b24a46a2ab0f5b853ba9b93f6562173e
ZenKhalil <feattzen@gmail.com>
2023-04-12 09:55:52 +0200

    displayAllBars works and shows array of data

--- app.js
+++ app.js
@@ -2,17 +2,22 @@
 
 window.addEventListener("load", start);
 
-const data = [35, 89, 11, 56, 73, 2, 8, 97, 29, 78, 23, 1, 80, 7, 64, 52, 47, 57, 50, 60, 25, 88, 86, 83, 58, 69, 4, 15, 51, 70, 91, 54, 61, 28, 90, 92, 14, 37, 24, 43, 72]
+const data = [19, 89, 11, 56, 73, 2, 8, 97, 29, 78, 23, 1, 80, 7, 64, 52, 47, 57, 50, 60, 25, 88, 86, 83, 58, 69, 4, 15, 51, 70, 91, 54, 61, 28, 90, 92, 14, 37, 24, 43, 72]
 
 
 function start() {
     console.log("JavaScript kører");
+    displayAllBars();
 
-    const bars = document.querySelectorAll(".bar")
-    const firstBar = bars[0];
+}
 
-    let height = data[0]/32*100;
+function displayAllBars() {
+     const bars = document.querySelectorAll(".bar")
 
-    firstBar.style.height = `${height}px`;
+     for(let i = 0; i < 40; i++) {
+        const bar = bars[i];
+        let height = data[i] / 32 * 100;
+        bar.style.height = `${height}px`;
+     }
 
 }
