0fc2e5a02f9612f8eb9b920e794c9657ed3cf961
ZenKhalil <feattzen@gmail.com>
2023-04-12 09:34:38 +0200

    Can change the first bar height

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+function start() {
+    console.log("JavaScript kører");
+
+    const bars = document.querySelectorAll(".bar")
+    const firstBar = bars[0];
+
+    let height = 40;
+
+    firstBar.style.height = `${height}px`;
+
+}
